39cf0fbc0fb3ba7f4ec114f4a40e043a48d908e3
tim <tim@studiodiip.com>
2025-06-28 18:23:26 +0200

    vial compiles with correct keymap

--- keyboards/handwired/cosmotim/keymaps/vial/keymap.c
+++ keyboards/handwired/cosmotim/keymaps/vial/keymap.c
@@ -1,8 +1,5 @@
 #include QMK_KEYBOARD_H
 
-#define LOWER MO(LAYER_LOWER)
-#define RAISE MO(LAYER_RAISE)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
@@ -10,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_LBRC,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                           KC_LGUI, KC_SPC,   LOWER,   RAISE,  KC_ENT, KC_RGUI
+                                           KC_LGUI, KC_SPC,   KC_RALT, KC_LCTL, KC_ENT, KC_RGUI
     ),
 
     [1] = LAYOUT(
